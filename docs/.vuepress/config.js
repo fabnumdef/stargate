8d3657054ffea114c6494b0e70ccead623cf82b4
Gaël Demette <gael.demette+git@qonfucius.team>
2020-02-25 15:55:28 +0100

    Add docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'Stargate docs',
+    base: '/stargate/stargate/',
+    dest: 'public',
+    themeConfig: {
+        navbar: false,
+        sidebar: [
+            '/',
+            '/technologies',
+            '/setup',
+        ]
+    },
+    locales: {
+        '/': {
+            lang: 'fr-FR',
+        },
+    },
+};
